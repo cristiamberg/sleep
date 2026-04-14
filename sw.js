fe84f7a51311e1b5063759beb34fb9d158f82053
Cristiamberg <ccristiamberg@gmail.com>
2026-04-13 21:33:15 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,18 @@
-const CACHE_NAME = 'sono-app-v1';
-const urlsToCache = [
-  './',
-  './index.html'
-];
-
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open('app-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html'
+      ]);
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Retorna a versão em cache ou faz o download da rede
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
